65024a1c462184a69ecbd8cd9e4b2f00f33c9661
Ganesh <115800415+Ganesh-Mk@users.noreply.github.com>
2024-05-07 22:41:25 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ function Login() {
   const handleSubmit = () => {
     axios.defaults.withCredentials = true;
     axios
-      .post('react-components-neon.vercel.app/login', { userEmail, userPassword })
+      .post('react-components-omega-jet.vercel.app/login', { userEmail, userPassword })
       .then((result) => console.log(result))
       .catch((err) => console.log(err))
   }
