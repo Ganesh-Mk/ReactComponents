00275a9c9212fa1753700bf0bccd3fc2e7c3ce82
Ganesh <115800415+Ganesh-Mk@users.noreply.github.com>
2024-05-07 22:14:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://react-components-primereact.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true,
   })
